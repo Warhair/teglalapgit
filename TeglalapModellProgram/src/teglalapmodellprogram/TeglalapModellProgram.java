cecd5d098e9e9806b3f503dab98dccf2b09942ff
Warhair <tegzesmarton@gmail.com>
2023-11-13 09:25:37 +0100

    teglalapalap

---
+++ TeglalapModellProgram/src/teglalapmodellprogram/TeglalapModellProgram.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package teglalapmodellprogram;
+
+/**
+ *
+ * @author tegzes.marton
+ */
+public class TeglalapModellProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
